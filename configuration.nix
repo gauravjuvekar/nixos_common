5a702583b860ac7b1a012d65eb4673f3d6ca87de
Gaurav Juvekar <gauravjuvekar@gmail.com>
2023-09-28 18:50:11 -0700

    Use cinnamon

--- configuration.nix
+++ configuration.nix
@@ -21,10 +21,10 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
+  # Enable the Cinnamon Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  
+  services.xserver.desktopManager.cinnamon.enable = true;
+
 
   # Enable sound.
   sound.enable = true;
