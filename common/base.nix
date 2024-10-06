82a08f29dfe59aa6865fc3037b2d58339f823286
Gaurav Juvekar <gauravjuvekar@gmail.com>
2024-10-05 19:26:43 -0700

    nix: prevent OOM

--- common/base.nix
+++ common/base.nix
@@ -12,4 +12,19 @@
       extra-experimental-features = flakes
       extra-experimental-features = nix-command
     '';
+
+    systemd =
+      {
+        slices."nix-daemon".sliceConfig =
+          {
+            ManagedOOMMemoryPressure = "kill";
+            ManagedOOMMemmoryPressureLimit = "90%";
+          };
+
+        services."nix-daemon".serviceConfig =
+          {
+            Slice = "nix-daemon.slice";
+            OOMScoreAdjust = 1000;
+          };
+      };
 }
