c475221a19a87187082c674ef00367a66cf31973
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-01-02 02:38:52 -0800

    sshd: add trusted ca keys

--- common/base_personal.nix
+++ common/base_personal.nix
@@ -1,4 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.fuse.userAllowOther = true;
+
+  services.openssh.extraConfig =
+    let
+      trusted_ca_keys = pkgs.concatTextFile
+        {
+          name = "sshd-ca-keys";
+          files =
+            [
+              ../keys/yk5a-22897261-ssh-signing-key.pub
+              ../keys/yk5c-24251755-ssh-signing-key.pub
+            ];
+          destination = "/ssh_trusted_ca_keys";
+
+        };
+    in
+    ''
+      TrustedUserCAKeys ${trusted_ca_keys}/ssh_trusted_ca_keys
+    '';
 }
