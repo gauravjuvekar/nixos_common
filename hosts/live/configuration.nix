c2ee5ab72f6eb973679510bb87bacb2a0042ad81
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-02-19 15:04:10 -0800

    live: add base packages and warp

--- hosts/live/configuration.nix
+++ hosts/live/configuration.nix
@@ -1,11 +1,19 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 {
   imports =
     [
       ../../common/base_etc.nix
+      ../../common/base_packages.nix
     ];
+
+  nixpkgs.config.allowUnfree = true;
 
   systemd.services."xe-daemon".enable = false;
 
   boot.kernelModules = [ "dm-raid" "dm-integrity" ];
+
+  environment.systemPackages = with pkgs;
+    [
+      cloudflare-warp
+    ];
 }
