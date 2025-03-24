4efe1f3b4c9ccc676d3edc45f5388a28fb9fc529
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-03-24 03:39:52 -0700

    deploy: add script

---
+++ deploy/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+FLAKE_REF="$1";
+TARGET_HOST="$2";
+
+nixos-anywhere \
+  --copy-host-keys \
+  --show-trace \
+  --disk-encryption-keys /tmp/cryptBoot.key ./remote/etc/keys/cryptBoot.key  \
+  --extra-files ./remote/ \
+  --target-host "$TARGET_HOST" \
+  --flake "$FLAKE_REF"
