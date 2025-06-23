b5e797c6f0e651664283bae9f0ad9510ea7a5d12
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-06-22 21:18:25 -0700

    wifi: add nv_visitor

--- common/networking_open_wifi.nix
+++ common/networking_open_wifi.nix
@@ -28,10 +28,35 @@
               addr-gen-mode = "default";
             };
           };
+          nv_visitor = {
+            connection = {
+              id = "NV_VISITOR";
+              permission = "";
+              type = "wifi";
+              # interface-name
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "NV_VISITOR";
+            };
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-psk";
+              psk-flags = "1"; # secrets from agent
+            };
+            ipv4 = {
+              method = "auto";
+            };
+            ipv6 = {
+              method = "auto";
+              addr-gen-mode = "default";
+            };
+          };
         };
       secrets.entries =
         let
           gaurav1 = config.networking.networkmanager.ensureProfiles.profiles.gaurav1;
+          nv_visitor = config.networking.networkmanager.ensureProfiles.profiles.nv_visitor;
         in
         [
           {
@@ -41,6 +66,13 @@
             matchSetting = "wifi-security";
             matchType = gaurav1.connection.type;
           }
+          {
+            file = config.age.secrets.wifi-passwd-nv_visitor.path;
+            key = "psk";
+            matchId = nv_visitor.connection.id;
+            matchSetting = "wifi-security";
+            matchType = nv_visitor.connection.type;
+          }
         ];
     };
 
@@ -48,5 +80,6 @@
   age.secrets =
     {
       wifi-passwd-gaurav1.rekeyFile = ../keys/secrets/wifi-passwd-gaurav1.age;
+      wifi-passwd-nv_visitor.rekeyFile = ../keys/secrets/wifi-passwd-nv_visitor.age;
     };
 }
--- hosts/gjuvekar-lt.client.nvidia.com/configuration.nix
+++ hosts/gjuvekar-lt.client.nvidia.com/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ ... }:
 {
   imports =
     [
@@ -6,6 +6,9 @@
       ../../common/base.nix
       ../../common/graphical.nix
       ../../common/interactive.nix
+      ../../common/networking_open_wifi.nix
       ../../common/physical_access.nix
     ];
+
+  age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUN3O9Pbm+oBEOeqcnsz/MESESfEkuGa5Nxp1YnpNj3 root@gjuvekar-lt";
 }
