0ce176d2889cf29d79a3d10b36d7d6b18798a539
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-11-02 19:31:16 -0800

    home-manager: merge flake.nix

--- flake.nix
+++ flake.nix
@@ -19,10 +19,32 @@
           url = "github:nix-community/disko";
           inputs.nixpkgs.follows = "nixpkgs";
         };
+      droid-sans-mono-dotted =
+        {
+          url = "github:gauravjuvekar/droid-sans-mono-dotted";
+          inputs.nixpkgs.follows = "nixpkgs";
+          inputs.flake-utils.follows = "flake-utils";
+        };
+      firefox-addons =
+        {
+          url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+          inputs.nixpkgs.follows = "nixpkgs";
+        };
+      fsaction =
+        {
+          url = "github:gauravjuvekar/fsaction";
+          inputs.nixpkgs.follows = "nixpkgs";
+          inputs.flake-utils.follows = "flake-utils";
+        };
       flake-utils =
         {
           url = "github:numtide/flake-utils";
           inputs.systems.follows = "systems";
+        };
+      home-manager =
+        {
+          url = "github:nix-community/home-manager";
+          inputs.nixpkgs.follows = "nixpkgs";
         };
       nixpkgs =
         {
@@ -105,6 +127,50 @@
       in
       {
         devShells.deploy = import ./deploy/shell.nix { inherit pkgs; };
+      }
+    )
+    // (
+      let
+        system = "x86_64-linux";
+        pkgs =
+          import inputs.nixpkgs {
+            system = system;
+            config =
+              {
+                allowUnfreePredicate =
+                  (
+                    pkgname: builtins.elem (inputs.nixpkgs.lib.getName pkgname)
+                      [
+                      ]
+                  );
+              };
+          };
+        extraSpecialArgs =
+          {
+            inputs = inputs;
+            firefox-addons = inputs.firefox-addons.outputs.packages.${system};
+            droid-sans-mono-dotted = inputs.droid-sans-mono-dotted.outputs.packages.${system};
+          };
+      in
+      {
+        homeConfigurations."gaurav@dt.sc.gjuvekar.com" =
+          inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            inherit extraSpecialArgs;
+            modules =
+              [
+                ./homes/hosts/gaurav-dt/home.nix
+              ];
+          };
+        homeConfigurations."gaurav@gaurav-nixlt" =
+          inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            inherit extraSpecialArgs;
+            modules =
+              [
+                ./homes/hosts/gaurav-nixlt/home.nix
+              ];
+          };
       }
     );
 }
