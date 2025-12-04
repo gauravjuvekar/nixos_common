f48a9b69f78356b37f1d452a52c7229e1f27c01f
Gaurav Juvekar <gauravjuvekar@gmail.com>
2025-12-04 03:31:53 -0800

    live: enable DoT by default

--- hosts/live/configuration.nix
+++ hosts/live/configuration.nix
@@ -16,6 +16,17 @@
   nixpkgs.config.allowUnfree = true;
 
   systemd.services."xe-daemon".enable = false;
+  networking.nameservers = [
+    "1.1.1.1#cloudflare-dns.com"
+    "8.8.8.8#dns.google"
+    "2606:4700:4700::1111#cloudflare-dns.com"
+    "2001:4860:4860::8888#dns.google"
+  ];
+
+  services.resolved = {
+    enable = true;
+    dnsovertls = "true";
+  };
 
   boot.kernelModules = [
     "btrfs"
