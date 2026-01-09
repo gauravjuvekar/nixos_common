da46d91c03a930bd6a60a142d257ea60101ebd25
Gaurav Juvekar <gauravjuvekar@gmail.com>
2026-01-09 10:14:32 +0530

    syncthing: move to modular config

--- homes/common/interactive_gjuvekar-com.nix
+++ homes/common/interactive_gjuvekar-com.nix
@@ -1,7 +1,6 @@
 { ... }:
 {
   imports = [
-    ./syncthing/syncthing.nix
     ./fsaction/fsaction.nix
   ];
 }
---
+++ src/common/packages/syncthing/default.nix
@@ -0,0 +1,59 @@
+{
+  config,
+  lib,
+  moduleContext,
+  osConfig ? null,
+  ...
+}:
+let
+  hostinfo =
+    {
+      home-manager = osConfig.hostinfo;
+      nixos-system = config.hostinfo;
+    }
+    ."${moduleContext}";
+in
+{
+  config =
+    let
+      domain_is_personal = lib.strings.hasSuffix ".gjuvekar.com" hostinfo.domainname;
+    in
+    {
+      "home-manager" = lib.mkIf (domain_is_personal && hostinfo.isLocalInteractive) {
+        services.syncthing = {
+          enable = true;
+          settings = {
+            devices = {
+              "dt.sc.gjuvekar.com" = {
+                id = "OR3NTZC-FJTRWZB-TDW43ZD-QY6O6M2-43ZBP37-653FL6D-JRTDGNU-SJY6HA4";
+              };
+              "lt2.roam.gjuvekar.com" = {
+                id = "YX5VZCP-7N3UV4H-YJDASJ5-WWYZB7E-B4WMZ2G-IPHAPIR-VOAZWSG-YPSDLA5";
+              };
+              "gaurav-p8" = {
+                id = "HXMXRPG-Q5RXPLZ-RJMPWPW-ILCA6JE-5VVUXAA-WL4FNTA-NCYWF2U-F3N7YQ5";
+              };
+              "gaurav-s5e" = {
+                id = "WZCRWVG-GUKHIPR-B5BV5MM-M63TPGT-QKD6IST-NOJYO4S-UQRKUXN-C643EAY";
+              };
+              "gaurav-tp" = {
+                id = "WG3P7FK-ZXS2LU7-BCF65RY-JIHMNFO-VFXRNQO-FTLZFW7-W4SUVKV-3OGFBAA";
+              };
+            };
+            folders = {
+              "${config.home.homeDirectory}/Sync" = {
+                id = "default";
+                devices = [
+                  "dt.sc.gjuvekar.com"
+                  "lt2.roam.gjuvekar.com"
+                ];
+              };
+            };
+          };
+        };
+      };
+      "nixos-system" = {
+      };
+    }
+    ."${moduleContext}";
+}
