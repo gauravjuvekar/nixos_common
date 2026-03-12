7252d4f075d80e5da9a06e6fd6ac2f543c6df445
Gaurav Juvekar <gauravjuvekar@gmail.com>
2026-03-12 07:05:30 -0700

    nvidia: switch to 580.142

--- hosts/dt.sc.gjuvekar.com/configuration.nix
+++ hosts/dt.sc.gjuvekar.com/configuration.nix
@@ -51,7 +51,13 @@
     open = true;
     modesetting.enable = true;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.production;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version            = "580.142";
+      sha256_64bit       = "sha256-IJFfzz/+icNVDPk7YKBKKFRTFQ2S4kaOGRGkNiBEdWM=";
+      openSha256         = "sha256-v968LbRqy8jB9+yHy9ceP2TDdgyqfDQ6P41NsCoM2AY=";
+      settingsSha256     = "sha256-BnrIlj5AvXTfqg/qcBt2OS9bTDDZd3uhf5jqOtTMTQM=";
+      persistencedSha256 = "sha256-0000000000000000000000000000000000000000000=";
+    };
   };
 
   programs.gnupg.agent = {
