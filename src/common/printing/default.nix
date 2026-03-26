00d47c0d366498bee464e887517584d94279634d
Gaurav Juvekar <gauravjuvekar@gmail.com>
2026-03-26 16:55:45 -0700

    printing: add ipp

--- src/common/printing/default.nix
+++ src/common/printing/default.nix
@@ -17,13 +17,13 @@ in
 {
   config =
     {
-      "home-manager" = lib.mkIf hostinfo.isLaptop {
+      "home-manager" = lib.mkIf hostinfo.isLocalGraphical {
         home.packages = [
           pkgs.simple-scan
         ];
       };
 
-      "nixos-system" = lib.mkIf hostinfo.isLaptop {
+      "nixos-system" = lib.mkIf hostinfo.isLocalGraphical {
         hardware.sane = {
           enable = true;
           brscan4.enable = true;
@@ -34,11 +34,19 @@ in
 
         services.ipp-usb.enable = true;
 
+        services.avahi = {
+          enable = true;
+          nssmdns4 = true;
+          openFirewall = true;
+        };
+
         services.printing = {
           enable = true;
           cups-pdf.enable = true;
           drivers = [
             pkgs.brlaser
+            pkgs.cups-browsed
+            pkgs.cups-filters
             pkgs.gutenprint
             pkgs.gutenprintBin
           ];
